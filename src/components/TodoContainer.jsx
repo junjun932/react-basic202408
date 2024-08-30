6b8bc0e676d59651d81d15f1c4a5017814177c00
junjun932 <1004ji93@gmail.com>
2024-08-30 18:22:58 +0900

    done

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,24 +1,12 @@
 import { TODO_SAMPLE } from "../constants/todo-sample";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 import { useState } from "react";
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState(TODO_SAMPLE);
-
-  const [text, setText] = useState("");
-
-  const onSubmit = (e) => {
-    e.preventDefault(); //이벤트의 기본 동작을 막는다
-
-    //새로운 todo 객체생성
-    const newTodo = {
-      id: crypto.randomUUID(), //랜덤한id생성
-      text, //text:text와 같은의미
-      isCompleted: false, //할일완료부분
-    };
-    //새로운 todo 객체를 todos 배열에 추가
-    setTodos([...todos, newTodo]);
-    //input 태그의 값을 초기화 -> 정리
-    setText("");
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos]);
   };
 
   const onToggleCompleted = (id) => {
@@ -46,25 +34,12 @@ const TodoContainer = () => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-        />
-        <button type="submit">추가</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.text} {todo.isCompleted ? "완료" : "미완료"}
-            <button onClick={() => onToggleCompleted(todo.id)}>
-              {todo.isCompleted ? "취소" : "완료"}
-            </button>
-            <button onClick={() => onDelete(todo.id)}>삭제</button>
-          </li>
-        ))}
-      </ul>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        onToggleCompleted={onToggleCompleted}
+        onDelete={onDelete}
+      />
     </div>
   );
 };
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault(); //이벤트의 기본 동작을 막는다
+
+    //새로운 todo 객체생성
+    const newTodo = {
+      id: crypto.randomUUID(), //랜덤한id생성
+      text, //text:text와 같은의미
+      isCompleted: false, //할일완료부분
+    };
+    //새로운 todo 객체를 todos 배열에 추가
+    addTodo(newTodo);
+    //input 태그의 값을 초기화 -> 정리
+    setText("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <button type="submit">추가</button>
+    </form>
+  );
+};
+
+export default TodoForm;
