2a4c5f9954f64397b8ab3641de616e28ee5107ef
junjun932 <1004ji93@gmail.com>
2024-08-30 17:02:27 +0900

    todo update

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -44,6 +44,7 @@ const TodoContainer = () => {
         {todos.map((todo) => (
           <li key={todo.id}>
             {todo.text} {todo.isCompleted ? "완료" : "미완료"}
+            <button>{todo.isCompleted ? "취소" : "완료"}</button>
             <button onClick={() => onDelete(todo.id)}>삭제</button>
           </li>
         ))}
